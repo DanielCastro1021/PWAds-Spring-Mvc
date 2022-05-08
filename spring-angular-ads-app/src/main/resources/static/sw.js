5df08ffbce93409d60dffcbaf8ec3940a6713f3d
Daniel Castro <danielcastrofafe@gmail.com>
2022-05-08 15:45:48 +0100

    PWA links and cache strategy changed

--- spring-angular-ads-app/src/main/resources/static/sw.js
+++ spring-angular-ads-app/src/main/resources/static/sw.js
@@ -1,12 +1,27 @@
 let CACHE_NAME = 'cache-v1';
+
+let externalAssets = [
+    'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js',
+    'https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js',
+    'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js',
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js'
+];
+
 let staticAssets = [
     'manifest.json',
     'index.html',
     'css/style.css',
     'html/fallout.html', 'html/navbar.html',
     'js/app.js', 'js/image-list.js',
-    'icons/ios/144.png', 'icons/ios/144.png'
+    'icons/ios/144.png', 'icons/ios/144.png',
+    'img/home-img-placeholder.png', 'img/broken-1.png',
+    'components/ads/list-ads.html', 'components/ads/list-ads.js',
+    'components/authentication/login.html', 'components/ads/list-ads.js',
+    'components/authentication/register.html', 'components/authentication/register.js'
 ];
+
+staticAssets.concat(externalAssets);
+
 self.addEventListener('install', (event) => {
     event.waitUntil(caches.open(CACHE_NAME).then((cache) => {
         return cache.addAll(staticAssets);
